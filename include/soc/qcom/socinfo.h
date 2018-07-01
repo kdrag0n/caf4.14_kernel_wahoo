6e9c96662ed432bf0dcec220b53f6b1001aeaf16
Angelo G. Del Regno <kholk11@gmail.com>
2018-07-01 12:58:12 +0200

    soc: qcom: socinfo: Add support for MSM8998
    Add IDs to support MSM8998 variant.
    
    [dl: Remove apq8098, sdm630, sdm636, sdm660, sdm845, and sda* variants]
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- include/soc/qcom/socinfo.h
+++ include/soc/qcom/socinfo.h
@@ -57,6 +57,8 @@
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,msm8996")
 #define early_machine_is_msm8996_auto()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,msm8996-cdp")
+#define early_machine_is_msm8998()	\
+	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,msm8998")
 #define early_machine_is_sm8150()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,sm8150")
 #define early_machine_is_sm8150p()	\
@@ -125,6 +127,7 @@
 #define early_machine_is_msm8916()	0
 #define early_machine_is_apq8084()	0
 #define early_machine_is_msm8996()	0
+#define early_machine_is_msm8998()	0
 #define early_machine_is_sm8150()	0
 #define early_machine_is_sm8150p()	0
 #define early_machine_is_sa8155()	0
@@ -169,6 +172,7 @@ enum msm_cpu {
 	MSM_CPU_8916,
 	MSM_CPU_8084,
 	MSM_CPU_8996,
+	MSM_CPU_8998,
 	MSM_CPU_SM8150,
 	MSM_CPU_SM8150P,
 	MSM_CPU_SA8155,
