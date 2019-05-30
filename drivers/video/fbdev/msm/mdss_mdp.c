2c4d6207d932ea46ebe33f23b013437b73fcfd1f
Angelo G. Del Regno <kholk11@gmail.com>
2019-05-30 09:25:44 +0200

    fbdev: msm: mdss_mdp: Return the right error code on regulator get fail
    On regulator get failure return the right error code so that
    if we fail with an -EPROBE_DEFER there, we will effectively
    defer the probe of the MDSS driver, instead of definitively
    failing the driver probe.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/video/fbdev/msm/mdss_mdp.c
+++ drivers/video/fbdev/msm/mdss_mdp.c
@@ -1794,9 +1794,10 @@ static int mdss_mdp_irq_clk_setup(struct mdss_data_type *mdata)
 
 	mdata->fs = devm_regulator_get(&mdata->pdev->dev, "vdd");
 	if (IS_ERR_OR_NULL(mdata->fs)) {
+		ret = PTR_ERR(mdata->fs);
 		mdata->fs = NULL;
 		pr_err("unable to get gdsc regulator\n");
-		return -EINVAL;
+		return ret;
 	}
 
 	mdata->venus = devm_regulator_get_optional(&mdata->pdev->dev,
