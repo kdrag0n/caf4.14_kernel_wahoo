32e27e56c00714356f117ab783142c5fb1666a18
Danny Lin <danny@kdrag0n.dev>
2020-06-24 16:05:36 -0700

    Revert "Revert "Revert "msm ipc compat"""
    This reverts commit d9b4d8b1d06525eb1f5bf05b6cfdee37501e4707.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- net/qrtr/qrtr.h
+++ net/qrtr/qrtr.h
@@ -3,19 +3,8 @@
 #define __QRTR_H_
 
 #include <linux/types.h>
-#include <linux/kthread.h>
-#include <net/sock.h>
-#include <uapi/linux/qrtr.h>
-
-#ifdef CONFIG_QRTR_IPC_ROUTER_COMPAT
-#include <uapi/linux/msm_ipc.h>
-#endif
 
 struct sk_buff;
-
-/* qrtr socket states */
-#define QRTR_STATE_MULTI	-2
-#define QRTR_STATE_INIT	-1
 
 /* endpoint node id auto assignment */
 #define QRTR_EP_NID_AUTO (-1)
@@ -45,86 +34,4 @@ void qrtr_endpoint_unregister(struct qrtr_endpoint *ep);
 int qrtr_endpoint_post(struct qrtr_endpoint *ep, const void *data, size_t len);
 
 int qrtr_peek_pkt_size(const void *data);
-
-/**
- * struct qrtr_node - endpoint node
- * @ep_lock: lock for endpoint management and callbacks
- * @ep: endpoint
- * @ref: reference count for node
- * @nid: node id
- * @net_id: network cluster identifer
- * @hello_sent: hello packet sent to endpoint
- * @qrtr_tx_flow: remote port tx flow control list
- * @resume_tx: wait until remote port acks control flag
- * @qrtr_tx_lock: lock for qrtr_tx_flow
- * @rx_queue: receive queue
- * @item: list item for broadcast list
- * @kworker: worker thread for recv work
- * @task: task to run the worker thread
- * @read_data: scheduled work for recv work
- * @say_hello: scheduled work for initiating hello
- * @ws: wakeupsource avoid system suspend
- * @ilc: ipc logging context reference
- */
-struct qrtr_node {
-	struct mutex ep_lock;
-	struct qrtr_endpoint *ep;
-	struct kref ref;
-	unsigned int nid;
-	unsigned int net_id;
-	atomic_t hello_sent;
-	atomic_t hello_rcvd;
-#ifdef CONFIG_QRTR_IPC_ROUTER_COMPAT
-	u32 service;
-	u32 instance;
-	u32 port;
-#endif
-
-	struct radix_tree_root qrtr_tx_flow;
-	struct wait_queue_head resume_tx;
-	struct mutex qrtr_tx_lock;	/* for qrtr_tx_flow */
-
-	struct sk_buff_head rx_queue;
-	struct list_head item;
-
-	struct kthread_worker kworker;
-	struct task_struct *task;
-	struct kthread_work read_data;
-	struct kthread_work say_hello;
-
-	struct wakeup_source *ws;
-
-	void *ilc;
-};
-
-struct qrtr_sock {
-	/* WARNING: sk must be the first member */
-	struct sock sk;
-	struct sockaddr_qrtr us;
-	struct sockaddr_qrtr peer;
-
-	int state;
-};
-
-static inline struct qrtr_sock *qrtr_sk(struct sock *sk)
-{
-	BUILD_BUG_ON(offsetof(struct qrtr_sock, sk) != 0);
-	return container_of(sk, struct qrtr_sock, sk);
-}
-
-extern unsigned int qrtr_local_nid;
-
-/* Protocol ops for use in msm_ipc_compat */
-int qrtr_bind(struct socket *sock, struct sockaddr *saddr, int len);
-int qrtr_sendmsg(struct socket *sock, struct msghdr *msg, size_t len);
-int qrtr_recvmsg(struct socket *sock, struct msghdr *msg, size_t size, int flags);
-int qrtr_connect(struct socket *sock, struct sockaddr *saddr, int len, int flags);
-int qrtr_release(struct socket *sock);
-
-/* Other functions for msm_ipc_compat */
-#ifdef CONFIG_QRTR_IPC_ROUTER_COMPAT
-int qrtr_msm_ipc_lookup_server(struct msm_ipc_server_info *srv_info,
-			       u32 service, u32 instance, int limit, u32 mask);
-#endif
-
 #endif
